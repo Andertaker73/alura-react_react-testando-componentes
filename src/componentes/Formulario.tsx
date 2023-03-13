7a74ce0a762ffee5aac6817da062dfacc9a654e7
Anderson Veeck <anderson.veeck@compasso.com.br>
2023-03-13 17:09:12 -0300

    Lesson One - done

---
+++ src/componentes/Formulario.tsx
@@ -0,0 +1,8 @@
+const Formulario = () => (
+  <form>
+    <input type='text' placeholder='Insira os nomes dos participantes' />
+    <button disabled={true}>Adicionar</button>
+  </form>
+)
+
+export default Formulario
